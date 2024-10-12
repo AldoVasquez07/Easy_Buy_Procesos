a61eb63fbcb0fa3212e6f1d4bfac1a28f0867064
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-10-12 15:03:10 -0500

    Corrigiendo Paginacion

--- easybuy/shop/views.py
+++ easybuy/shop/views.py
@@ -8,63 +8,22 @@ def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-
-    query = request.GET.get('q')
-    if query:
-        products = products.filter(name__icontains=query)
-
+    
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
 
-    # Cambiar la paginación a 12 productos por página
-    paginator = Paginator(products, 12)  # Aquí cambiamos a 12
+    # Agregar paginación aquí (12 productos por página)
+    paginator = Paginator(products, 12)  # 12 productos por página
     page = request.GET.get('page')
+    products = paginator.get_page(page)
     
-    try:
-        products = paginator.page(page)
-    except PageNotAnInteger:
-        products = paginator.page(1)
-    except EmptyPage:
-        products = paginator.page(paginator.num_pages)
-
-    return render(request, 
-                  'shop/product/list.html', 
-                  {'category': category,
-                   'categories': categories,
-                   'products': products,
-                   'query': query})
-
-    category = None
-    categories = Category.objects.all()
-    products = Product.objects.filter(available=True)
-
-    query = request.GET.get('q')
-    if query:
-        products = products.filter(name__icontains=query)
-
-    if category_slug:
-        category = get_object_or_404(Category, slug=category_slug)
-        products = products.filter(category=category)
-
-    # Agregar paginación (10 productos por página)
-    paginator = Paginator(products, 12)
-    page = request.GET.get('page')
+    return render(request, 'shop/product/list.html', {
+        'category': category,
+        'categories': categories,
+        'products': products,
+    })
     
-    try:
-        products = paginator.page(page)
-    except PageNotAnInteger:
-        products = paginator.page(1)
-    except EmptyPage:
-        products = paginator.page(paginator.num_pages)
-
-    return render(request, 
-                  'shop/product/list.html', 
-                  {'category': category,
-                   'categories': categories,
-                   'products': products,
-                   'query': query})
-
 
 def product_detail(request, id, slug):
     product = get_object_or_404(Product, id=id, slug=slug, available=True)

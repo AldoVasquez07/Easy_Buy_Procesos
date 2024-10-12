33b14dc440292d303090c66e365fa04b6522f50f
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-10-12 14:57:52 -0500

    Ajustando muestreo de Ofertas

--- easybuy/shop/views.py
+++ easybuy/shop/views.py
@@ -1,3 +1,4 @@
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
@@ -15,9 +16,50 @@ def product_list(request, category_slug=None):
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
+
+    # Cambiar la paginación a 12 productos por página
+    paginator = Paginator(products, 12)  # Aquí cambiamos a 12
+    page = request.GET.get('page')
     
-    return render(request,
-                  'shop/product/list.html',
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, 
+                  'shop/product/list.html', 
+                  {'category': category,
+                   'categories': categories,
+                   'products': products,
+                   'query': query})
+
+    category = None
+    categories = Category.objects.all()
+    products = Product.objects.filter(available=True)
+
+    query = request.GET.get('q')
+    if query:
+        products = products.filter(name__icontains=query)
+
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
+
+    # Agregar paginación (10 productos por página)
+    paginator = Paginator(products, 12)
+    page = request.GET.get('page')
+    
+    try:
+        products = paginator.page(page)
+    except PageNotAnInteger:
+        products = paginator.page(1)
+    except EmptyPage:
+        products = paginator.page(paginator.num_pages)
+
+    return render(request, 
+                  'shop/product/list.html', 
                   {'category': category,
                    'categories': categories,
                    'products': products,

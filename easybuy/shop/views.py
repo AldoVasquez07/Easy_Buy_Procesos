c7cdc0c58406b7e0ca6403a659473930777911ec
Aldo Ray Vasquez Lopez <avasquezl@ulasalle.edu.pe>
2024-10-12 22:53:42 -0500

    Arreglando busqueda

--- easybuy/shop/views.py
+++ easybuy/shop/views.py
@@ -2,27 +2,30 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.shortcuts import render, get_object_or_404
 from .models import Category, Product
 from cart.forms import CartAddProductForm
+from django.shortcuts import render, get_object_or_404
+from .models import Product, Category
 
 
 def product_list(request, category_slug=None):
     category = None
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
-    
+
+    # Agregar búsqueda por nombre
+    query = request.GET.get('q')
+    if query:
+        products = products.filter(name__icontains=query)
+
     if category_slug:
         category = get_object_or_404(Category, slug=category_slug)
         products = products.filter(category=category)
-
-    # Agregar paginación aquí (12 productos por página)
-    paginator = Paginator(products, 12)  # 12 productos por página
-    page = request.GET.get('page')
-    products = paginator.get_page(page)
     
-    return render(request, 'shop/product/list.html', {
-        'category': category,
-        'categories': categories,
-        'products': products,
-    })
+    return render(request,
+                  'shop/product/list.html',
+                  {'category': category,
+                   'categories': categories,
+                   'products': products,
+                   'query': query})  # Pasar el query al template
     
 
 def product_detail(request, id, slug):
